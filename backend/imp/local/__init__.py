b515db3df38087cf19befe8737c9d36b79fe1326
Ivan Lorenzo <ivan.lorenzo@madisonmk.com>
2021-12-28 08:59:23 +0100

    refactor local reader

--- backend/imp/local/__init__.py
+++ backend/imp/local/__init__.py
@@ -1,11 +1,14 @@
-import csv
+import pandas as pd
 
 from backend.imp import BaseImporter
 
 
-class Local(BaseImporter):
+class LocalImporter(BaseImporter):
 
     def read_historic_data(self):
-        file_directory = self.file
-        with open(file_directory) as file_csv:
-            return csv.reader(file_csv)
+        pd.read_csv(self.file)
+
+    def get_data_info(self):
+        check_offline = self._is_existing_ticket() and self._is_existing_ticket()
+        if check_offline:
+            return self.read_historic_data()
