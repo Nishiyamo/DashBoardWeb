e18c15f949423230865e4b8600599a312414a2e5
Ivan Lorenzo <ivan.lorenzo@madisonmk.com>
2021-12-28 08:17:15 +0100

    date status checker

--- backend/imp/__init__.py
+++ backend/imp/__init__.py
@@ -1,5 +1,6 @@
 import os.path
 from datetime import datetime
+import pandas as pd
 
 from backend.settings import ROOT_FILE_DIRECTORY
 
@@ -8,13 +9,16 @@ class BaseImporter:
 
     # By default this importer is going to take the values from the actual year from 'yahoo' api of Amazon ticker
     def __init__(self, index=None, start_date=None, end_date=None):
-        self.file = ROOT_FILE_DIRECTORY + f'/ticker_{index}.csv'
-        self.index = index or 'AMZN'
+        self.index = index or 'aapl'
         self.start_date = start_date or datetime(datetime.today().year, 1, 1).strftime("%Y-%m-%d")
         self.end_date = end_date or datetime.today().strftime("%Y-%m-%d")
+        self.file = ROOT_FILE_DIRECTORY + f'/ticker_{self.index}.csv'
 
     def _is_existing_ticket(self):
         return os.path.isfile(self.file)
 
     def _is_updated_ticket(self):
-        return
+        ticket_data = pd.read_csv(self.file)
+        last_line_date = ticket_data.tail(1)
+        last_line_date = last_line_date['Date']
+        return last_line_date < self.end_date
